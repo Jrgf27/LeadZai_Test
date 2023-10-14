68481f6d6854a7494d9daf721ef5c8f8be95180c
Joao Fernandes <ricardo.jfernandes27@gmail.com>
2023-10-14 11:50:53 +0100

    added docstrings to pagination method

--- pagination_gen.py
+++ pagination_gen.py
@@ -1,3 +1,15 @@
+"""Pagination generator module"""
 
 def pagination_generator(currrent_page: int, total_pages: int, boundaries: int, around: int) -> str:
+    """
+    Outputs the pagination string given the current page, total pages, boundaries 
+    and around values.
+    
+    current_page: In which page the user is located, needs to be positive integer.
+    total_pages: How many pages exist, needs to be positive integer.
+    boundaries: How many pages are shown before and after the first and last pages (inclusive),
+    needs to be positive integer.
+    around: How many pages are visible around the current page (exclusive), 
+    needs to be either positive integer or zero.
+    """
     print(currrent_page, total_pages, boundaries, around)

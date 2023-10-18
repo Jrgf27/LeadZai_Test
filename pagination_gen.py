b54599075d15695a1b7f08d09f53a9f9ab062c3b
Joao Fernandes <ricardo.jfernandes27@gmail.com>
2023-10-18 22:34:06 +0100

    Improved code based on feedback

--- pagination_gen.py
+++ pagination_gen.py
@@ -13,91 +13,110 @@ def pagination_generator(current_page: int, total_pages: int, boundaries: int, a
     needs to be either positive integer or zero.
     """
 
-    #Checking if input parameters are the correct type
-    if not isinstance(current_page, int) or isinstance(current_page, bool):
-        raise TypeError("current_page must be a positive integer")
-    if not isinstance(total_pages, int) or isinstance(total_pages, bool):
-        raise TypeError("total_pages must be a positive integer")
-    if not isinstance(boundaries, int) or isinstance(boundaries, bool):
-        raise TypeError("boundaries must be a positive integer")
-    if not isinstance(around, int) or isinstance(around, bool):
-        raise TypeError("around must be a positive integer or zero")
+    #Input variable checks start#
 
-    #Check if input parameter are within appropriate values
-    if total_pages <= 0:
-        raise ValueError("total_pages must be a positive integer")
-    if current_page <= 0 or current_page > total_pages:
-        raise ValueError("current_page must be a positive integer, lower than total_pages")
-    if boundaries <= 0 or boundaries > total_pages:
-        raise ValueError("boundaries must be a positive integer, lower than total_pages")
-    if around < 0 or around > total_pages:
-        raise ValueError("around must be a positive integer or zero, lower than total_pages")
+    check_if_int('current_page', current_page)
+    check_if_int('total_pages', total_pages)
+    check_if_int('boundaries', boundaries)
+    check_if_int('around', around)
 
-    #If the boundaries value is atleast half of the total pages value,
-    # the start and end page number lists will crossover and generate one single list.
-    # Thus a list with all possible page numbers is immediately returned
-    if boundaries > total_pages//2:
-        result = ' '.join(map(str, range(1, total_pages + 1)))
-        print (result)
-        return result
+    check_for_value_error('current_page', current_page, False)
+    check_for_value_error('total_pages', total_pages, False)
+    check_for_value_error('boundaries', boundaries, True)
+    check_for_value_error('around', around, True)
 
-    #If the current page value and the around values are high enough to cover all page values
-    #return the list with all page numbers
-    if (current_page-around)<=1 and (current_page+around)>=total_pages:
-        result = ' '.join(map(str, range(1, total_pages + 1)))
-        print (result)
-        return result
+    if current_page > total_pages:
+        raise ValueError("current_page must be lower than total_pages")
 
-    #Generation of list of pages around the start page
-    start_pagination=list(range(1, 1 + boundaries))
+    #Input variable checks end#
 
-    #Generation of list of pages around the current page, if around = 0 created list will be empty
-    #and will default to [current_page]
-    current_pagination = [i for i in range(current_page - around, current_page + around + 1) \
-                            if 0 < i <=total_pages] or [current_page]
+    if boundaries > total_pages // 2:
+        return convert_list_to_string(map(str, range(1, total_pages + 1)))
 
-    #Extending the start page list with the current page list
+    if (current_page-around) <= 1 and (current_page+around) >= total_pages:
+        return convert_list_to_string(map(str, range(1, total_pages + 1)))
+
+    if around == 0:
+        current_pagination = [current_page]
+    else:
+        current_page_start = max(1, current_page - around)
+        current_page_end = min(total_pages, current_page + around)
+        current_pagination = list(range(current_page_start, current_page_end + 1))
+
+    if boundaries == 0:
+        if current_pagination[0] == 1:
+            result_list = current_pagination
+            result_list.append('...')
+        elif current_pagination[-1] == total_pages:
+            result_list = ['...']
+            result_list.extend(current_pagination)
+        else:
+            result_list = ['...']
+            result_list.extend(current_pagination)
+            result_list.append('...')
+
+        return convert_list_to_string(result_list)
+
+    start_pagination = list(range(1, 1 + boundaries))
+    end_pagination = list(range(total_pages - boundaries + 1, total_pages + 1))
+
+    if check_list_inside_list(parent_list = end_pagination, child_list = current_pagination):
+        result_list = pagination_list_extendor(start_pagination, end_pagination)
+        return convert_list_to_string(result_list)
+
     result_list = pagination_list_extendor(start_pagination, current_pagination)
-
-    #Checking if list already has all page numbers possible and should be returned
-    if result_list[-1]==total_pages:
-        result = ' '.join(map(str, result_list))
-        print(result)
-        return result
-
-    #Generation of list of pages around the final page
-    end_pagination=list(range(total_pages, total_pages - boundaries, -1))[::-1]
-
-    #Extending current result list with end page list,
-    # converting to str and returning formatted string
     result_list = pagination_list_extendor(result_list, end_pagination)
-    result = ' '.join(map(str, result_list))
-    print(result)
-    return result
+    return convert_list_to_string(result_list)
+
+def check_for_value_error(variable_name: str, variable_value: int, can_be_zero: bool) -> None:
+    """Checking for value error on input variables"""
+    if can_be_zero:
+        if variable_value < 0:
+            raise ValueError(variable_name + "must be a positive integer or zero")
+    else:
+        if variable_value <= 0:
+            raise ValueError(variable_name + "must be a positive integer")
+
+def check_if_int(variable_name:str, variable_value:int) -> None:
+    """Checking for type error for each input variable"""
+    if isinstance(variable_value, bool):
+        raise TypeError(variable_name + "must be a positive integer")
+    if not isinstance(variable_value, int):
+        raise TypeError(variable_name + "must be a positive integer")
+
+def convert_list_to_string(list_to_convert:list) -> str:
+    """
+    Converts input list into a string with each list element separated
+    by a space.
+    """
+
+    converted_list_to_string = ' '.join(map(str, list_to_convert))
+    print(converted_list_to_string)
+    return converted_list_to_string
+
+def check_list_inside_list(parent_list:list, child_list:list) -> bool:
+    """
+    Checking if child_list values are present in parent_list
+    """
+    return set(child_list).issubset(set(parent_list))
 
 def pagination_list_extendor(start_list:list, target_list:list) -> list:
     """
-    Method to check pagination lists for crossover between two input lists.
+    Method to check for crossover between two input lists.
     If crossover exists, returned list will be the extension of start_list with target_list.
     If no crossover occurs, returned list will be the extension of start_list with target_list, 
     with a '...' element separating the two lists.
     """
 
-    #Checking if target_list is already inside start_list, if it is return start list
-    if set(target_list).issubset(set(start_list)):
+    if check_list_inside_list(parent_list = start_list, child_list = target_list):
         return start_list
 
-    start_last_value=start_list[-1]
-
-    #If last value of first list is in target list,
-    #obtain index of value and extend first list with second list, starting at index
+    start_last_value = start_list[-1]
     if start_last_value in target_list:
         index = target_list.index(start_last_value) + 1
         start_list.extend(target_list[index::])
-    #If the start of the second list is the first list final value + 1, simply extend
     elif (start_last_value + 1) == target_list[0]:
         start_list.extend(target_list)
-    #If neither conditions are true, append a '...' element and extend
     else:
         start_list.append('...')
         start_list.extend(target_list)
--- test_pagination_generator.py
+++ test_pagination_generator.py
@@ -38,35 +38,10 @@ class TestPaginationGenerator(TestCase):
         self.assertRaises(TypeError, pg, 2, 2, None, 2)
         self.assertRaises(TypeError, pg, 2, 2, 2, None)
 
-    def test_dict_values(self):
-        """Test that TypeError is raised when input values are type dict"""
-        test_dict={'key':1}
-        self.assertRaises(TypeError, pg, test_dict, 2, 2, 2)
-        self.assertRaises(TypeError, pg, 2, test_dict, 2, 2)
-        self.assertRaises(TypeError, pg, 2, 2, test_dict, 2)
-        self.assertRaises(TypeError, pg, 2, 2, 2, test_dict)
-
-    def test_list_values(self):
-        """Test that TypeError is raised when input values are type list"""
-        test_list=[0,1,2]
-        self.assertRaises(TypeError, pg, test_list, 2, 2, 2)
-        self.assertRaises(TypeError, pg, 2, test_list, 2, 2)
-        self.assertRaises(TypeError, pg, 2, 2, test_list, 2)
-        self.assertRaises(TypeError, pg, 2, 2, 2, test_list)
-
-    def test_set_values(self):
-        """Test that TypeError is raised when input values are type set"""
-        test_set={0,1,2}
-        self.assertRaises(TypeError, pg, test_set, 2, 2, 2)
-        self.assertRaises(TypeError, pg, 2, test_set, 2, 2)
-        self.assertRaises(TypeError, pg, 2, 2, test_set, 2)
-        self.assertRaises(TypeError, pg, 2, 2, 2, test_set)
-
     def test_zero_values(self):
         """Test that ValueError is raised when input values are zero"""
         self.assertRaises(ValueError, pg, 0, 2, 2, 2)
         self.assertRaises(ValueError, pg, 2, 0, 2, 2)
-        self.assertRaises(ValueError, pg, 2, 2, 0, 2)
 
     def test_negative_values(self):
         """Test that ValueError is raised when input values are negative"""
@@ -76,33 +51,77 @@ class TestPaginationGenerator(TestCase):
         self.assertRaises(ValueError, pg, 2, 2, test_negative, 2)
         self.assertRaises(ValueError, pg, 2, 2, 2, test_negative)
 
-    def test_boundaries_higher_than_total_pages(self):
-        """Test that ValueError is raised when boundaries are higher than total pages"""
-        self.assertRaises(ValueError, pg, 2, 2, 3, 2)
-
     def test_current_page_higher_than_total_pages(self):
         """Test that ValueError is raised when current page is higher than total pages"""
         self.assertRaises(ValueError, pg, 3, 2, 2, 2)
 
-    def test_around_higher_than_total_pages(self):
-        """Test that ValueError is raised when around is higher than total pages"""
-        self.assertRaises(ValueError, pg, 2, 2, 2, 3)
-
-    def test_valid_full_list(self):
-        """Test that the output is the correct string given the input values"""
-        self.assertEqual(pg(4,5,5,0), '1 2 3 4 5')
+    def test_valid_full_list_current_page_at_end(self):
+        """Test with valid values and current page at the end of pagination"""
+        self.assertEqual(pg(5,5,5,0), '1 2 3 4 5')
         self.assertEqual(pg(4,5,1,5), '1 2 3 4 5')
-        self.assertEqual(pg(4,5,5,5), '1 2 3 4 5')
+        self.assertEqual(pg(3,3,1,1), '1 2 3')
+
+    def test_valid_full_list_current_page_at_start(self):
+        """Test with valid values and current page at the start of pagination"""
         self.assertEqual(pg(2,5,3,0), '1 2 3 4 5')
-        self.assertEqual(pg(4,5,1,2), '1 2 3 4 5')
-        self.assertEqual(pg(2,3,1,0), '1 2 3')
         self.assertEqual(pg(1,3,1,1), '1 2 3')
 
-    def test_valid_list_with_dots(self):
-        """"Tests which output lists with '...' inside"""
+    def test_valid_full_list_around_value_zero(self):
+        """Test with valid values and around value as zero"""
+        self.assertEqual(pg(5,5,5,0), '1 2 3 4 5')
+        self.assertEqual(pg(5,5,3,0), '1 2 3 4 5')
+        self.assertEqual(pg(1,3,2,0), '1 2 3')
+
+    def test_valid_full_list_around_large_value(self):
+        """Test with valid values and around value as a large integer"""
+        self.assertEqual(pg(5,5,1,1000), '1 2 3 4 5')
+        self.assertEqual(pg(1,3,1,1000), '1 2 3')
+
+    def test_valid_full_list_boundaries_value_zero(self):
+        """Test with valid values and boundaries value as zero"""
+        self.assertEqual(pg(5,5,0,5), '1 2 3 4 5')
+        self.assertEqual(pg(1,3,0,3), '1 2 3')
+
+    def test_valid_full_list_boundaries_large_value(self):
+        """Test with valid values and boundaries value as a large integer"""
+        self.assertEqual(pg(5,5,1000,0), '1 2 3 4 5')
+        self.assertEqual(pg(1,3,1000,0), '1 2 3')
+
+    def test_valid_list_with_dots_current_page_at_end(self):
+        """"Tests which output lists with '...' inside and current page at end of list"""
         self.assertEqual(pg(4,5,1,0), '1 ... 4 5')
-        self.assertEqual(pg(2,5,2,0), '1 2 ... 4 5')
         self.assertEqual(pg(4,10,2,2), '1 2 3 4 5 6 ... 9 10')
+
+        self.assertEqual(pg(10,10,3,1), '1 2 3 ... 8 9 10')
+        self.assertEqual(pg(9,10,4,1), '1 2 3 4 ... 7 8 9 10')
+
+    def test_valid_list_with_dots_current_page_at_start(self):
+        """"Tests which output lists with '...' inside and current page at start of list"""
+        self.assertEqual(pg(2,5,2,0), '1 2 ... 4 5')
+        self.assertEqual(pg(2,10,3,1), '1 2 3 ... 8 9 10')
+        self.assertEqual(pg(1,10,4,1), '1 2 3 4 ... 7 8 9 10')
+
+    def test_valid_list_with_dots_current_page_at_middle(self):
+        """"Tests which output lists with '...' inside and current page at middle of list"""
         self.assertEqual(pg(4,10,1,1), '1 ... 3 4 5 ... 10')
         self.assertEqual(pg(5,10,2,1), '1 2 ... 4 5 6 ... 9 10')
+
+    def test_valid_list_with_dots_large_values(self):
+        """Test which outputs a valid list given large total and current pages values"""
         self.assertEqual(pg(500,1000,2,1), '1 2 ... 499 500 501 ... 999 1000')
+
+    def test_valid_list_with_dots_around_value_zero(self):
+        """Test which output lists with '...' inside and around value as zero"""
+        self.assertEqual(pg(5,10,1,0), '1 ... 5 ... 10')
+        self.assertEqual(pg(1,10,1,0), '1 ... 10')
+        self.assertEqual(pg(2,10,1,0), '1 2 ... 10')
+        self.assertEqual(pg(9,10,1,0), '1 ... 9 10')
+        self.assertEqual(pg(10,10,1,0), '1 ... 10')
+
+    def test_valid_list_with_dots_boundary_value_zero(self):
+        """Test which output lists with '...' inside and boundary value as zero"""
+        self.assertEqual(pg(5,10,0,0), '... 5 ...')
+        self.assertEqual(pg(1,10,0,0), '1 ...')
+        self.assertEqual(pg(1,10,0,2), '1 2 3 ...')
+        self.assertEqual(pg(10,10,0,0), '... 10')
+        self.assertEqual(pg(9,10,0,2), '... 7 8 9 10')
